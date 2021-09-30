70c7ca0d0eb286a7094987e83720a3d9a7defa71
Jake Johnson <56829096+jaketajohnson@users.noreply.github.com>
2021-09-30 13:49:25 -0500

    Logging abstraction

--- Template.py
+++ Template.py
@@ -16,85 +16,28 @@
 import arcpy
 import logging
 import os
-import sys
 import traceback
+import sys
+sys.path.insert(0, "Y:/Scripts")
+import Logging
 
 
-def ScriptLogging():
-    """Enables console and log file logging; see test script for comments on functionality"""
-    current_directory = os.getcwd()
-    script_filename = os.path.basename(sys.argv[0])
-    log_filename = os.path.splitext(script_filename)[0]
-    log_file = os.path.join(current_directory, f"{log_filename}.log")
-    if not os.path.exists(log_file):
-        with open(log_file, "w"):
-            pass
-    message_formatting = "%(asctime)s - %(levelname)s - %(message)s"
-    date_formatting = "%Y-%m-%d %H:%M:%S"
-    formatter = logging.Formatter(fmt=message_formatting, datefmt=date_formatting)
-    logging_output = logging.getLogger(f"{log_filename}")
-    logging_output.setLevel(logging.INFO)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    logging_output.addHandler(console_handler)
-    logging.basicConfig(format=message_formatting, datefmt=date_formatting, filename=log_file, filemode="w", level=logging.INFO)
-    return logging_output
-
-
-def Template():
+def template():
     """Create lateral lines using PACP observations"""
 
-    # Logging
-    def logging_lines(name):
-        """Use this wrapper to insert a message before and after the function for logging purposes"""
-        if type(name) == str:
-            def logging_decorator(function):
-                def logging_wrapper(*exception):
-                    logger.info(f"{name} Start")
-                    function(*exception)
-                    logger.info(f"{name} Complete")
-                return logging_wrapper
-            return logging_decorator
-
-    logger = ScriptLogging()
-    logger.info("Script Execution Start")
-
-    """Paths"""
+    # Paths
 
     # Environment
     arcpy.env.overwriteOutput = True
 
-    @logging_lines("Template logging message")
+    @Logging.insert("Template logging message")
     def template_function():
-        return print("Template output")
+        """Template description"""
 
-    # Try running above scripts
-    try:
-        template_function()
-    except (IOError, KeyError, NameError, IndexError, TypeError, UnboundLocalError, ValueError):
-        traceback_info = traceback.format_exc()
-        try:
-            logger.info(traceback_info)
-        except NameError:
-            print(traceback_info)
-    except arcpy.ExecuteError:
-        try:
-            logger.error(arcpy.GetMessages(2))
-        except NameError:
-            print(arcpy.GetMessages(2))
-    except:
-        logger.exception("Picked up an exception!")
-    finally:
-        try:
-            logger.info("Script Execution Complete")
-        except NameError:
-            pass
-
-
-def main():
-    Template()
+        Logging.logger.info("------START Template")
+        print("Template output")
+        Logging.logger.info("------FINISH Template")
 
 
 if __name__ == '__main__':
-    main()
+    template()
